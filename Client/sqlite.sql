5f8b2410abe0edaaa3166cd9028f8b277c54f2d4
Alireza Noorian <alir431@gmail.com>
2011-01-27 01:53:40 +0330

    added client database

---
+++ Client/sqlite.sql
@@ -0,0 +1,21 @@
+create table users (
+	id integer primary key not null,
+	nationalid integer not null,
+	quality integer not null default "0",
+	firstname varchar(255) not null,
+	lastname varchar(255) not null,
+	birth_date date not null,
+	address varchar(255) null default null,
+	phone varchar(50) null default null,
+	gender integer not null,
+	description varchar(255) null default null,
+	email varchar(255) default null collate "ascii_bin",
+	userpass char(40) default null collate "ascii_bin",
+
+	created_at datetime null default null,
+	score integer not null default "0",
+	correction_time integer not null default "0",
+
+	unique (email) on conflict abort,
+	unique (nationalid) on conflict abort
+);
