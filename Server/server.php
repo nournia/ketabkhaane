a9866bd27bbfa3ee9805832ae6f8a33162f26d19
Alireza Noorian <alir431@gmail.com>
2011-03-18 21:23:37 +0330

    added insert or update operation for received data

--- Server/server.php
+++ Server/server.php
@@ -28,32 +28,33 @@ function getQueryValue($value)
 ?>
 <?php
 	connectDatabase();
-	
-	/*
+
+/*
 	$result = mysql_query('select * from users');
 	while ($row = mysql_fetch_row($result))
 		print_r($row);
 	die;
-	 */
-
-	// get posted json data and decode them
-	$json = $_POST['json']; // utf8_decode($_POST['json']);
-	$data = json_decode($json);
-	
-	foreach ($data as $table => $rows)
-	foreach($rows as $row)
-	{
-		$values = '';
-		foreach($row as $column)
-			$values .= getQueryValue($column) .',';
-		$values .= 'NOW()'; // updated_at field
-			
-		//$qry = "insert into {$table} values ({$values})";
-		//echo $qry;
+*/
 		
-		mysql_query("insert into {$table} values ({$values})");
-	}
-	
+	if (! empty($_POST['json']))
+	{
+		// get posted json data and decode them
+		$json = $_POST['json']; // utf8_decode($_POST['json']);
+		$data = json_decode($json);
+		
+		foreach ($data as $table => $rows)
+		foreach($rows as $row)
+		{
+			$values = '';
+			foreach($row as $column)
+				$values .= getQueryValue($column) .',';
+			$values .= 'NOW()'; // updated_at field
+				
+			mysql_query("replace into {$table} values ({$values})");
+		}
+	} else
+		echo 'invalid access';
+		
 	disconnectDatabase();
 ?>
 </pre>
