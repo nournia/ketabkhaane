135908ff12cc4577985e0d952c42013474cf6cf1
Alireza Noorian <alir431@gmail.com>
2011-03-29 12:45:01 +0430

    added application information

--- Client/reghaabat/main.cpp
+++ Client/reghaabat/main.cpp
@@ -8,6 +8,14 @@ int main(int argc, char *argv[])
     Connector::connectDb();
 
     QApplication a(argc, argv);
+
+    a.setStyle("plastique");
+
+    a.setApplicationName("Reghaabat");
+    a.setApplicationVersion("0.1");
+    a.setOrganizationName("Rooyesh");
+    a.setOrganizationDomain("www.Reghaabat.ir");
+
     MainWindow w;
     w.show();
 
