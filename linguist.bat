6702458fa1bccd7dcf70a801e72828734c527b91
Alireza Nourian <alireza.nournia@gmail.com>
2012-05-05 18:55:13 +0430

    add linguist script

---
+++ linguist.bat
@@ -0,0 +1,5 @@
+cd "d:\repo\reghaabat"
+D:\Qt\qt\bin\lupdate.exe reghaabat.pro
+D:\Qt\qt\bin\linguist.exe reghaabat_fa.ts
+D:\Qt\qt\bin\lrelease.exe reghaabat_fa.ts
+D:\Qt\qt\bin\lrelease.exe qt_fa.ts
